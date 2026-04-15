44adca2ffa9651719d9904797687cbf7c6971ba1
Daria Bondarchuk <dariabondarchuk.work@gmail.com>
2026-04-15 15:16:46 +0300

    Modify location parser. Add new patterns. Remove the unnecessary.

--- src/parsers/location_parser.py
+++ src/parsers/location_parser.py
@@ -4,12 +4,28 @@ from .parser_config import PARSER_CONFIG
 from .models import Location
 import time
 
+# TODO 
+# create oblast config to normalize oblast
+# cut off oblast and try to catch settlement name
+# return array: [{oblast: ..., settlement: ...}]
+# handle multiple matchesof settlements i.e. Бпла в напрямку Бахмача, БпЛА на півдні Полтавщини
+# handle errors 
+# settlememt/oblast name normalization
 class LocationParser: 
     def __init__(self):
 
+        prepositions = [
+            'на', 'у', 'в', 'до', 'від', 'поблизу', 'біля', 'коло', 'під', 'через',
+            'на н.п.', 'над н.п.', 'поблизу н.п.', 'в районі н.п.', 'в районі', 'в напрямку н.п.',
+            'повз', 'межі', 'в напрямку', 'в напрямі', 'над', 'на сході', 'на півночі',
+            'на півдні', 'на північ', 'на південь', 'на захід',
+            'на схід', 'південніше', 'північніше', 'східніше', 'західніше', 'на/повз', 'у бік',
+            'півночі', 'півдня', 'сходу', 'заходу'
+        ]
+
         # Direction patterns
         self.direction_pattern = re.compile(
-            r'(?:на|з|із|зі|від|у напрямку|курс)\s+'
+            r'(?:на|з|із|зі|від|у напрямку|курс|курс на)\s+'
             r'(північ(?:н[а-я]+)?|схід(?:н[а-я]+)?|південь|південн[а-я]+|'
             r'захід(?:н[а-я]+)?|західн[а-я]+|'
             r'північно-східн[а-я]+|південно-східн[а-я]+|'
@@ -17,6 +33,25 @@ class LocationParser:
             re.IGNORECASE
         )
 
+        prepositions_sorted = sorted(prepositions, key=len, reverse=True)
+        prepositions_pattern = '|'.join(re.escape(prep) for prep in prepositions_sorted)
+
+        # Threat + settlement/region/oblast
+        self.threat_location_pattern = re.compile(
+            r'(бпла|БпЛА|Каб|КАБ|керованих авіаційних бомб)(?:\s+[\u0400-\u04FF]{{1,30}}){{0,3}}\s+(?:{prepositions})\s+([\u0410-\u042F][\u0400-\u04FF]+(?:\s+[\u0410-\u042F][\u0400-\u04FF]+)*)(?:\s+(?:обл\.|область|області|район|районі|р-н|р-ні|р-н\.))'
+        )
+
+        # Generic preposition and settlement pattern
+        # Matches: повз Київ/на Харків
+        self.direction_settlement_pattern = re.compile(
+            rf'(?:{prepositions_pattern})\s+([\u0410-\u042F][\u0400-\u04FF]+(?:\s+[\u0410-\u042F][\u0400-\u04FF]+)*)'
+        )
+
+        # Full oblast/region pattern
+        # Matches: Сумська область, Харківська область, Білоцерківському районі
+        self.oblast_full_pattern = re.compile(
+            r'([\u0410-\u042F][\u0400-\u04FF\s]{2,30})\s+(?:обл.|область|області|район|районі|р-н|р-ні|р-н)'
+        )
 
         # Oblast pattern 
         # Matches: Сумщині, Харківщині, Дніпропетровщині, etc.
@@ -24,19 +59,11 @@ class LocationParser:
             r'\b([А-ЯІЇЄҐ][а-яіїєґ]+(?:ськ|цьк|зьк)?(?:щин|ччин)[іаиуюї]?)\b'
         )
 
-
-        # Full oblast pattern
-        # Matches: Сумська область, Харківська область
-        self.oblast_full_pattern = re.compile(
-            r'\b([А-ЯІЇЄҐ][а-яіїєґ]+(?:ська|цька|зька)) область\b'
-        )
-
         # City patterns 
         # Matches: на Суми, на Охтирку, на Дніпро
         self.city_on_pattern = re.compile(
             r'\bна\s+([А-ЯІЇЄҐ][а-яіїєґ\']+(?:(?:-[А-ЯІЇЄҐ][а-яіїєґ]+)|(?:ськ[а-яіїєґ]*)|(?:цьк[а-яіїєґ]*))?[а-яіїєґу]?)\b'
         )
-
 
         self.geolocator = Nominatim(user_agent="telegram_scraper_v1")
         self.patterns = []
@@ -72,23 +99,18 @@ class LocationParser:
         return None
 
 
+# TODO make some patterns optional
     def get_location(self, text):
         locations = set()
 
+        # parse settlement
+        matches =  self.direction_settlement_pattern.findall(text)
+        locations.update(matches)
+        
         # parse oblast 
         for match in self.oblast_pattern.finditer(text): 
             oblast = match.group(1)
             locations.add(oblast)
 
-        for match in self.oblast_full_pattern.finditer(text): 
-            oblast = match.group(1)
-            locations.add(oblast)
-
-        # parse city
-        for match in self.city_on_pattern.finditer(text): 
-            city = match.group(1)
-            locations.add(city)
-
         print(locations)
-
         return list(locations)
