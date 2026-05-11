f4a2b65ae23b9d709f87a14758678bbb1d39e826
Daria Bondarchuk <dariabondarchuk.work@gmail.com>
2026-05-11 23:01:02 +0300

    Additions and reorganization of the oblast normalization config

--- src/parsers/oblast_config.py
+++ src/parsers/oblast_config.py
@@ -1,21 +1,22 @@
 OBLASTS = {
-    "vinnytsia": {
-        "name_uk": "Вінницька область",
-        "name_en": "Vinnytsia Oblast",
+    "chernihiv": {
+        "name_uk": "Чернігівська область",
+        "name_en": "Chernihiv Oblast",
         "relat_stems": [
-            "вінницьк", "віннич", "вінниц"
+            "чернігів", "ніжин", "прилуки", "бахмач", "новгород-сіверський", "корюківка"
         ],
-        "center": {"lat": 49.2328, "lon": 28.4810},
-        "bbox": {"north": 49.9167, "south": 48.0500, "west": 27.6167, "east": 29.9833}
+        "center": {"lat": 51.4982, "lon": 31.2893},
+        "bbox": {"north": 52.3667, "south": 50.6000, "west": 30.2667, "east": 33.0667}
     },
-    "volyn": {
-        "name_uk": "Волинська область",
-        "name_en": "Volyn Oblast",
+    "sumy": {
+        "name_uk": "Сумська область",
+        "name_en": "Sumy Oblast",
         "relat_stems": [
-            "нововолин", "волин", "луцьк", "ковель"
+            "сумськ", "сумщ", "суми", "конотоп", "лебедин", "костопіль", 
+            "шостк", "охтирк", "охтирськ", "ромнинськ", "ромни", "ромнів", "глухів", "глухов", "сум"
         ],
-        "center": {"lat": 50.7472, "lon": 25.3254},
-        "bbox": {"north": 51.6000, "south": 50.1500, "west": 23.7833, "east": 26.2333}
+        "center": {"lat": 50.9077, "lon": 34.7981},
+        "bbox": {"north": 51.9667, "south": 50.1500, "west": 32.5833, "east": 35.7500}
     },
     "dnipropetrovsk": {
         "name_uk": "Дніпропетровська область",
@@ -31,11 +32,102 @@ OBLASTS = {
         "name_uk": "Донецька область",
         "name_en": "Donetsk Oblast",
         "relat_stems": [
-            "донецьк", "донбас", "донеччин", "краматорськ", "крам", "марган", 
+            "донецьк", "донбас", "донеччин", "краматорськ", "марган", 
             "покровськ", "дружківк"
         ],
         "center": {"lat": 48.0159, "lon": 37.8029},
         "bbox": {"north": 49.1167, "south": 46.9667, "west": 36.5833, "east": 38.9167}
+    },
+    "kharkiv": {
+        "name_uk": "Харківська область",
+        "name_en": "Kharkiv Oblast",
+        "relat_stems": [
+            "харків", "лозов", "ізюм", "чугуїв", "чугуєв", "первомайськ", "балаклі", 
+            "куп'янськ", "красноград", "мереф", "богодух", "гадяч", "горішні плавні"
+        ],
+        "center": {"lat": 49.9935, "lon": 36.2304},
+        "bbox": {"north": 50.4667, "south": 48.9167, "west": 35.6667, "east": 37.9000}
+    },
+    "kherson": {
+        "name_uk": "Херсонська область",
+        "name_en": "Kherson Oblast",
+        "relat_stems": [
+            "херсон", "каховк", "олешки", "генічеськ", "скадовськ"
+        ],
+        "center": {"lat": 46.6354, "lon": 32.6169},
+        "bbox": {"north": 47.2333, "south": 45.8833, "west": 32.4500, "east": 34.3667}
+    },
+    "cherkasy": {
+        "name_uk": "Черкаська область",
+        "name_en": "Cherkasy Oblast",
+        "relat_stems": [
+            "черкаськ", "черкащ", "черкас", "канов", "канів", "умань", "сміл", "золотонош"
+        ],
+        "center": {"lat": 49.4444, "lon": 32.0598},
+        "bbox": {"north": 49.9500, "south": 48.6833, "west": 30.8833, "east": 32.7167}
+    },
+    "zaporizhzhia": {
+        "name_uk": "Запорізька область",
+        "name_en": "Zaporizhzhia Oblast",
+        "relat_stems": [
+            "запоріжж", "запорізьк"
+        ],
+        "center": {"lat": 47.8388, "lon": 35.1396},
+        "bbox": {"north": 48.3167, "south": 46.5667, "west": 34.3833, "east": 36.8333}
+    },
+    "mykolaiv": {
+        "name_uk": "Миколаївська область",
+        "name_en": "Mykolaiv Oblast",
+        "relat_stems": [
+            "миколаїв"
+        ],
+        "center": {"lat": 46.9750, "lon": 31.9946},
+        "bbox": {"north": 47.8500, "south": 46.3000, "west": 30.9333, "east": 33.2833}
+    },
+    "odesa": {
+        "name_uk": "Одеська область",
+        "name_en": "Odesa Oblast",
+        "relat_stems": [
+            "одеськ", "одещин", "одес", "чорноморськ",
+            "ізмаїл", "білгород-дністровськ", "подільськ"
+        ],
+        "center": {"lat": 46.4825, "lon": 30.7233},
+        "bbox": {"north": 47.8167, "south": 45.1833, "west": 28.6000, "east": 31.1500}
+    },
+    "poltava": {
+        "name_uk": "Полтавська область",
+        "name_en": "Poltava Oblast",
+        "relat_stems": [
+            "полтавськ", "полтавщ", "полтав", "кременчук", 
+            "кременчуц", "лубнинськ", "лубни", "миргород", "гадяч", "горішні плавні"
+        ],
+        "center": {"lat": 49.5883, "lon": 34.5514},
+        "bbox": {"north": 50.5667, "south": 48.7333, "west": 32.5167, "east": 35.3000}
+    },
+    "kyiv": {
+        "name_uk": "Київська область",
+        "name_en": "Kyiv Oblast",
+        "relat_stems": [
+            "київськ", "київщ", "біла церква", "білоцерківськ", "бровар", "фастів",
+            "фастов", "ірпін", "вишгород", 
+        ],
+        "center": {"lat": 50.0521, "lon": 30.7617},
+        "bbox": {"north": 51.3500, "south": 49.2000, "west": 29.3167, "east": 31.9000}
+    },
+    "kyiv_city": {
+        "name_uk": "Київ",
+        "name_en": "Kyiv",
+        "informal": ["Столиця"],
+        "variations": [
+            "Київ", "Киев", "місто Київ",
+            "Kyiv", "Kiev"
+        ],
+        "relat_stems": [
+            "київ"
+        ],
+        "center": {"lat": 50.4501, "lon": 30.5234},
+        "bbox": {"north": 50.5900, "south": 50.2133, "west": 30.2392, "east": 30.8250},
+        "is_city": True  # Special case: Kyiv has city status
     },
     "zhytomyr": {
         "name_uk": "Житомирська область",
@@ -46,23 +138,89 @@ OBLASTS = {
         "center": {"lat": 50.2547, "lon": 28.6587},
         "bbox": {"north": 51.5333, "south": 49.4333, "west": 27.3833, "east": 29.5333}
     },
-    "zakarpattia": {
-        "name_uk": "Закарпатська область",
-        "name_en": "Zakarpattia Oblast",
+    "vinnytsia": {
+        "name_uk": "Вінницька область",
+        "name_en": "Vinnytsia Oblast",
         "relat_stems": [
-            "закарпатт", "ужгород", "берегов", "мукачев", "хуст"
+            "вінницьк", "віннич", "вінниц"
         ],
-        "center": {"lat": 48.6208, "lon": 22.2879},
-        "bbox": {"north": 48.5833, "south": 47.9000, "west": 22.1333, "east": 24.6167}
+        "center": {"lat": 49.2328, "lon": 28.4810},
+        "bbox": {"north": 49.9167, "south": 48.0500, "west": 27.6167, "east": 29.9833}
     },
-    "zaporizhzhia": {
-        "name_uk": "Запорізька область",
-        "name_en": "Zaporizhzhia Oblast",
+    "kirovohrad": {
+        "name_uk": "Кіровоградська область",
+        "name_en": "Kirovohrad Oblast",
         "relat_stems": [
-            "запоріжж", "запорізьк"
+            "кіровоградськ", "кіровоградщин", "кропивницьк"
         ],
-        "center": {"lat": 47.8388, "lon": 35.1396},
-        "bbox": {"north": 48.3167, "south": 46.5667, "west": 34.3833, "east": 36.8333}
+        "center": {"lat": 48.5079, "lon": 32.2623},
+        "bbox": {"north": 49.4333, "south": 47.5500, "west": 30.9833, "east": 33.4667}
+    },
+    "luhansk": {
+        "name_uk": "Луганська область",
+        "name_en": "Luhansk Oblast",
+        "relat_stems": [
+            "луганськ", "луганщ"
+        ],
+        "center": {"lat": 48.5740, "lon": 39.3078},
+        "bbox": {"north": 49.9167, "south": 47.7333, "west": 37.9833, "east": 40.2167}
+    },
+    "lviv": {
+        "name_uk": "Львівська область",
+        "name_en": "Lviv Oblast",
+        "relat_stems": [
+            "львів", "дрогобич", "самбір", "стрий", "трускавець", "трускавц",
+            "самбор", "червоноград", "стрия", "борислав"
+        ],
+        "center": {"lat": 49.8397, "lon": 24.0297},
+        "bbox": {"north": 50.5333, "south": 48.9500, "west": 22.5167, "east": 25.1833}
+    },
+    "rivne": {
+        "name_uk": "Рівненська область",
+        "name_en": "Rivne Oblast",
+        "relat_stems": [
+            "рівне", "дубненськ", "дубно", "вараськ", "вараш", "костопіль", 
+            "сарни", "здолбунів"
+        ],
+        "center": {"lat": 50.6199, "lon": 26.2516},
+        "bbox": {"north": 51.4333, "south": 50.0333, "west": 24.7667, "east": 27.4500}
+    },
+    "ternopil": {
+        "name_uk": "Тернопільська область",
+        "name_en": "Ternopil Oblast",
+        "relat_stems": [
+            "тернопіль"
+        ],
+        "center": {"lat": 49.5535, "lon": 25.5948},
+        "bbox": {"north": 50.1000, "south": 48.8833, "west": 24.7167, "east": 26.4333}
+    },
+    "khmelnytskyi": {
+        "name_uk": "Хмельницька область",
+        "name_en": "Khmelnytskyi Oblast",
+        "relat_stems": [
+            "хмельницьк", "хмельнич", "нетішин", "шепетівк", "камянець-подільськ", "славут",
+            "старокостянтин", "старкон", "красилів", "красилов", "дунаївц"
+        ],
+        "center": {"lat": 49.4229, "lon": 26.9871},
+        "bbox": {"north": 50.4000, "south": 48.6333, "west": 26.0833, "east": 27.8667}
+    },
+    "chernivtsi": {
+        "name_uk": "Чернівецька область",
+        "name_en": "Chernivtsi Oblast",
+        "relat_stems": [
+            "чернівец", "чернівц", "новодністровськ", "хотин", "сокиряни", "умань", "заставн", "сторожинець"
+        ],
+        "center": {"lat": 48.2921, "lon": 25.9358},
+        "bbox": {"north": 48.5000, "south": 47.7500, "west": 25.1500, "east": 26.6500}
+    },
+    "volyn": {
+        "name_uk": "Волинська область",
+        "name_en": "Volyn Oblast",
+        "relat_stems": [
+            "нововолин", "волин", "луцьк", "ковель"
+        ],
+        "center": {"lat": 50.7472, "lon": 25.3254},
+        "bbox": {"north": 51.6000, "south": 50.1500, "west": 23.7833, "east": 26.2333}
     },
     "ivano-frankivsk": {
         "name_uk": "Івано-Франківська область",
@@ -74,206 +232,13 @@ OBLASTS = {
         "center": {"lat": 48.9226, "lon": 24.7111},
         "bbox": {"north": 49.0667, "south": 47.8833, "west": 23.5000, "east": 25.8500}
     },
-    "kyiv": {
-        "name_uk": "Київська область",
-        "name_en": "Kyiv Oblast",
+    "zakarpattia": {
+        "name_uk": "Закарпатська область",
+        "name_en": "Zakarpattia Oblast",
         "relat_stems": [
-            "київськ", "київщ", "біла церква", "білоцерківськ", "бровар", "фастів",
-            "ірпін", "вишгород", 
+            "закарпатт", "ужгород", "берегов", "мукачев", "хуст"
         ],
-        "center": {"lat": 50.0521, "lon": 30.7617},
-        "bbox": {"north": 51.3500, "south": 49.2000, "west": 29.3167, "east": 31.9000}
-    },
-    "kirovohrad": {
-        "name_uk": "Кіровоградська область",
-        "name_en": "Kirovohrad Oblast",
-        "informal": ["Кіровоградщина", "Kropyvnytskyi Oblast"],
-        "variations": [
-            "Кіровоградська область", "Кіровоградська обл", "Кіровоградська обл.",
-            "Кіровоградщина",
-            "Kirovohrad Oblast", "Kirovohradska oblast", "Kropyvnytskyi Oblast"
-        ],
-        "center": {"lat": 48.5079, "lon": 32.2623},
-        "bbox": {"north": 49.4333, "south": 47.5500, "west": 30.9833, "east": 33.4667}
-    },
-    "luhansk": {
-        "name_uk": "Луганська область",
-        "name_en": "Luhansk Oblast",
-        "informal": ["Луганщина"],
-        "variations": [
-            "Луганська область", "Луганська обл", "Луганська обл.",
-            "Луганщина",
-            "Luhansk Oblast", "Lugansk Oblast", "Luhanska oblast"
-        ],
-        "center": {"lat": 48.5740, "lon": 39.3078},
-        "bbox": {"north": 49.9167, "south": 47.7333, "west": 37.9833, "east": 40.2167}
-    },
-    "lviv": {
-        "name_uk": "Львівська область",
-        "name_en": "Lviv Oblast",
-        "informal": ["Львівщина", "Галичина"],
-        "variations": [
-            "Львівська область", "Львівська обл", "Львівська обл.",
-            "Львівщина", "Галичина",
-            "Lviv Oblast", "Lvivska oblast"
-        ],
-        "center": {"lat": 49.8397, "lon": 24.0297},
-        "bbox": {"north": 50.5333, "south": 48.9500, "west": 22.5167, "east": 25.1833}
-    },
-    "mykolaiv": {
-        "name_uk": "Миколаївська область",
-        "name_en": "Mykolaiv Oblast",
-        "informal": ["Миколаївщина"],
-        "variations": [
-            "Миколаївська область", "Миколаївська обл", "Миколаївська обл.",
-            "Миколаївщина",
-            "Mykolaiv Oblast", "Mykolaivska oblast", "Nikolaev Oblast"
-        ],
-        "center": {"lat": 46.9750, "lon": 31.9946},
-        "bbox": {"north": 47.8500, "south": 46.3000, "west": 30.9333, "east": 33.2833}
-    },
-    "odesa": {
-        "name_uk": "Одеська область",
-        "name_en": "Odesa Oblast",
-        "informal": ["Одещина"],
-        "variations": [
-            "Одеська область", "Одеська обл", "Одеська обл.",
-            "Одещина",
-            "Odesa Oblast", "Odeska oblast", "Odessa Oblast"
-        ],
-        "center": {"lat": 46.4825, "lon": 30.7233},
-        "bbox": {"north": 47.8167, "south": 45.1833, "west": 28.6000, "east": 31.1500}
-    },
-    "poltava": {
-        "name_uk": "Полтавська область",
-        "name_en": "Poltava Oblast",
-        "informal": ["Полтавщина"],
-        "variations": [
-            "Полтавська область", "Полтавська обл", "Полтавська обл.",
-            "Полтавщина",
-            "Poltava Oblast", "Poltavska oblast"
-        ],
-        "center": {"lat": 49.5883, "lon": 34.5514},
-        "bbox": {"north": 50.5667, "south": 48.7333, "west": 32.5167, "east": 35.3000}
-    },
-    "rivne": {
-        "name_uk": "Рівненська область",
-        "name_en": "Rivne Oblast",
-        "informal": ["Рівненщина"],
-        "variations": [
-            "Рівненська область", "Рівненська обл", "Рівненська обл.",
-            "Рівненщина",
-            "Rivne Oblast", "Rivnenska oblast", "Rovno Oblast"
-        ],
-        "center": {"lat": 50.6199, "lon": 26.2516},
-        "bbox": {"north": 51.4333, "south": 50.0333, "west": 24.7667, "east": 27.4500}
-    },
-    "sumy": {
-        "name_uk": "Сумська область",
-        "name_en": "Sumy Oblast",
-        "informal": ["Сумщина"],
-        "variations": [
-            "Сумська область", "Сумська обл", "Сумська обл.",
-            "Сумщина",
-            "Sumy Oblast", "Sumska oblast"
-        ],
-        "center": {"lat": 50.9077, "lon": 34.7981},
-        "bbox": {"north": 51.9667, "south": 50.1500, "west": 32.5833, "east": 35.7500}
-    },
-    "ternopil": {
-        "name_uk": "Тернопільська область",
-        "name_en": "Ternopil Oblast",
-        "informal": ["Тернопільщина"],
-        "variations": [
-            "Тернопільська область", "Тернопільська обл", "Тернопільська обл.",
-            "Тернопільщина",
-            "Ternopil Oblast", "Ternopilska oblast"
-        ],
-        "center": {"lat": 49.5535, "lon": 25.5948},
-        "bbox": {"north": 50.1000, "south": 48.8833, "west": 24.7167, "east": 26.4333}
-    },
-    "kharkiv": {
-        "name_uk": "Харківська область",
-        "name_en": "Kharkiv Oblast",
-        "informal": ["Харківщина", "Слобожанщина"],
-        "variations": [
-            "Харківська область", "Харківська обл", "Харківська обл.",
-            "Харківщина", "Слобожанщина",
-            "Kharkiv Oblast", "Kharkivska oblast", "Kharkov Oblast"
-        ],
-        "center": {"lat": 49.9935, "lon": 36.2304},
-        "bbox": {"north": 50.4667, "south": 48.9167, "west": 35.6667, "east": 37.9000}
-    },
-    "kherson": {
-        "name_uk": "Херсонська область",
-        "name_en": "Kherson Oblast",
-        "informal": ["Херсонщина"],
-        "variations": [
-            "Херсонська область", "Херсонська обл", "Херсонська обл.",
-            "Херсонщина",
-            "Kherson Oblast", "Khersonska oblast"
-        ],
-        "center": {"lat": 46.6354, "lon": 32.6169},
-        "bbox": {"north": 47.2333, "south": 45.8833, "west": 32.4500, "east": 34.3667}
-    },
-    "khmelnytskyi": {
-        "name_uk": "Хмельницька область",
-        "name_en": "Khmelnytskyi Oblast",
-        "informal": ["Хмельниччина", "Поділля"],
-        "variations": [
-            "Хмельницька область", "Хмельницька обл", "Хмельницька обл.",
-            "Хмельниччина", "Поділля",
-            "Khmelnytskyi Oblast", "Khmelnitska oblast"
-        ],
-        "center": {"lat": 49.4229, "lon": 26.9871},
-        "bbox": {"north": 50.4000, "south": 48.6333, "west": 26.0833, "east": 27.8667}
-    },
-    "cherkasy": {
-        "name_uk": "Черкаська область",
-        "name_en": "Cherkasy Oblast",
-        "informal": ["Черкащина"],
-        "variations": [
-            "Черкаська область", "Черкаська обл", "Черкаська обл.",
-            "Черкащина",
-            "Cherkasy Oblast", "Cherkaska oblast"
-        ],
-        "center": {"lat": 49.4444, "lon": 32.0598},
-        "bbox": {"north": 49.9500, "south": 48.6833, "west": 30.8833, "east": 32.7167}
-    },
-    "chernivtsi": {
-        "name_uk": "Чернівецька область",
-        "name_en": "Chernivtsi Oblast",
-        "informal": ["Буковина"],
-        "variations": [
-            "Чернівецька область", "Чернівецька обл", "Чернівецька обл.",
-            "Буковина",
-            "Chernivtsi Oblast", "Chernivetska oblast"
-        ],
-        "center": {"lat": 48.2921, "lon": 25.9358},
-        "bbox": {"north": 48.5000, "south": 47.7500, "west": 25.1500, "east": 26.6500}
-    },
-    "chernihiv": {
-        "name_uk": "Чернігівська область",
-        "name_en": "Chernihiv Oblast",
-        "informal": ["Чернігівщина", "Сіверщина"],
-        "variations": [
-            "Чернігівська область", "Чернігівська обл", "Чернігівська обл.",
-            "Чернігівщина", "Сіверщина",
-            "Chernihiv Oblast", "Chernihivska oblast", "Chernigov Oblast"
-        ],
-        "center": {"lat": 51.4982, "lon": 31.2893},
-        "bbox": {"north": 52.3667, "south": 50.6000, "west": 30.2667, "east": 33.0667}
-    },
-    "kyiv_city": {
-        "name_uk": "Київ",
-        "name_en": "Kyiv",
-        "informal": ["Столиця"],
-        "variations": [
-            "Київ", "Киев", "місто Київ",
-            "Kyiv", "Kiev"
-        ],
-        "center": {"lat": 50.4501, "lon": 30.5234},
-        "bbox": {"north": 50.5900, "south": 50.2133, "west": 30.2392, "east": 30.8250},
-        "is_city": True  # Special case: Kyiv has city status
+        "center": {"lat": 48.6208, "lon": 22.2879},
+        "bbox": {"north": 48.5833, "south": 47.9000, "west": 22.1333, "east": 24.6167}
     }
 }
